49e9f08cb8e559a5e54d21b57d3bc3253d29eb24
Alex Eduardo Chiaranda <alexec@nexyon.com.br>
2016-01-27 10:37:37 -0200

    adicionando META para python3 only

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.2',
+    version='0.2.3',
 
     description='Busca endereço a partir de um CEP no site dos Correios',
     long_description=long_description,
@@ -55,6 +55,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
