df7c9cec1909ac112fb8349d831e5b7806e9c986
Alex Eduardo Chiaranda <aechiara@gmail.com>
2016-03-10 09:54:14 -0300

    test the service for match

--- tests.py
+++ tests.py
@@ -6,8 +6,7 @@ import buscacep as busca
 
 class TestBuscaCep(unittest.TestCase):
     def setUp(self):
-        pass
-        # self.resp = {'bairro': 'Bela Vista', 'cep': '01310-000', 'localidade': 'São Paulo/SP', 'logradouro': 'Avenida Paulista - até 610 - lado par'}
+        self.resp = {'bairro': 'Bela Vista', 'cep': '01310-000', 'localidade': 'São Paulo/SP', 'logradouro': 'Avenida Paulista - até 610 - lado par'}
 
     def test_cep_not_none(self):
         with self.assertRaises(AttributeError):
@@ -17,6 +16,10 @@ class TestBuscaCep(unittest.TestCase):
         with self.assertRaises(AttributeError):
             busca.busca_cep_correios(12345678)
 
+    def test_cep_match(self):
+        self.r = busca.busca_cep_correios('01310000').as_dict()
+        self.resultado = set(self.resp.items()) ^ set(self.r.items())
+        self.assertEquals(0, len(self.resultado))
 
 if __name__ == '__main__':
     unittest.main()
